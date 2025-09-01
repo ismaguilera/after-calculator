6d569012fa806f418cd2be154b8068378015c728
Ismael Aguilera <iaguiler@ug.uchile.cl>
2025-09-01 19:14:32 -0400

    fix(dev): Corrige la carga de traducciones en el entorno local Resumen del problema:
    Al ejecutar la aplicación en el entorno de desarrollo local, las traducciones de i18next no se cargaban. En su lugar, la interfaz de usuario mostraba las claves de traducción (por ejemplo, controlPanel.title) en lugar del texto real.
    
    Se ha actualizado vite.config.ts para que la opción base se establezca dinámicamente según el modo de ejecución de Vite:
    
    En modo production, se usará la ruta relativa '/after-calculator/', correcta para el despliegue en GitHub Pages.
    En modo development, se usará '/', asegurando que los recursos se carguen desde la raíz del servidor de desarrollo local.
    Este cambio soluciona el problema de las traducciones y no afecta al proceso de build para producción. La modificación en package-lock.json refleja las dependencias actualizadas.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig} from 'vite';
+import path from 'path';
+import { defineConfig, loadEnv} from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  base: 'https://ismaguilera.github.io/after-calculator/', // Link page
-})
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  return {
+    plugins: [react()],
+    base: mode === 'production' ? '/after-calculator/' : '/',
+
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, '.'),
+      }
+    }
+  };
+});
